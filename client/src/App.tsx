2b7489e44f02b768a8f0df2b829937ceb0f77ed0
jaswasin <jaswant.vision@gmail.com>
2026-02-13 19:46:31 +0530

    Fix: configure wouter router with base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,7 +6,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
+// Get the base path from vite config
+const basePath = "/jaswant-singh-site";
+
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -18,12 +21,14 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <Router base={basePath}>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <AppRouter />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
